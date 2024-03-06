406fe0e20eb0c1cd52d035a017fed748844380a5
devpark <devpark@feelanet.com>
2024-03-06 16:31:26 +0900

    change folder route

--- app/(platform)/VDimages/_components/data-table.tsx
+++ app/(platform)/VDimages/_components/data-table.tsx
@@ -25,8 +25,8 @@ import {
   TableRow,
 } from '@/components/ui/table';
 
-import { DataTablePagination } from './data-table-pagination';
-import { DataTableToolbar } from './data-table-toolbar';
+import { DataTablePagination } from './table/data-table-pagination';
+import { DataTableToolbar } from './table/data-table-toolbar';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
