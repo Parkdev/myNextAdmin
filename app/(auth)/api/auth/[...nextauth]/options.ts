e7bc8c66c57c55f88f847b61691008973e65c000
devpark <devpark@feelanet.com>
2024-03-27 15:20:40 +0900

    awake credentialsprovider

--- app/(auth)/api/auth/[...nextauth]/options.ts
+++ app/(auth)/api/auth/[...nextauth]/options.ts
@@ -2,7 +2,7 @@ import { DefaultSession, Session, NextAuthOptions } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
 
 import AzureADProvider from 'next-auth/providers/azure-ad';
-// import CredentialsProvider from 'next-auth/providers/credentials';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 declare module 'next-auth' {
   interface Session extends DefaultSession {
@@ -46,28 +46,28 @@ export const nextAuthOptions: NextAuthOptions = {
     //azure-ad&state=X-7HTx8cXPGAq4MAas9Nb5DAUVlEmAXAhk1rKCoqF-Q
 
     //로그인 이름과 비밀번호로 로그인 (임시)
-    // CredentialsProvider({
-    //   name: 'Credentials',
-    //   credentials: {
-    //     username: { label: 'Username', type: 'text', placeholder: 'Username' },
-    //     password: { label: 'Password', type: 'password' },
-    //   },
-    //   async authorize(credentials) {
-    //     const user = {
-    //       id: '42',
-    //       name: 'test1',
-    //       password: 'test!',
-    //     };
-    //     if (
-    //       credentials?.username === user.name &&
-    //       credentials?.password === user.password
-    //     ) {
-    //       return user;
-    //     } else {
-    //       return null;
-    //     }
-    //   },
-    // }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'Username' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: '42',
+          name: 'test',
+          password: 'test',
+        };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
   callbacks: {
     async jwt({ token, account }): Promise<JWT> {
